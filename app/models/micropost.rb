51e165196a1af9020423d5bfc481b7958cccad2c
pgblanco <pablogustavo.blanco@gmail.com>
2014-11-07 17:24:55 -0300

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,17 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  
   default_scope -> { order('created_at DESC') }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
+
+  def Micropost.from_users_followed_by(user)
+    following_ids = user.following_ids
+    where("user_id IN (?) OR user_id = ?", following_ids, user)
+  end
+
 
   private
 
